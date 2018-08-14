e0b77329a74e77efc5cd9b54888a83a440384038
ManasaMona <manasaalisetty@gmail.com>
2018-08-14 16:13:39 +0530

    " -> 3 of 3 passed. pylint: 4.59/10 "

--- Student_demo/CodeCampPoker/sample.py
+++ Student_demo/CodeCampPoker/sample.py
@@ -1,8 +1,11 @@
-def sum0fDigits(n):
-	sum=0
-	while n:
-		sum += n%10
-		n//=10
-	return sum
+# def sum0fDigits(n):
+# 	sum=0
+# 	while n:
+# 		sum += n%10
+# 		n//=10
+# 	return sum
 
-print (max([300,456,12345],key = sum0fDigits) )
+# print (max([300,456,12345],key = sum0fDigits) )
+
+lst = [ i for i in "hello"]
+print(lst)
